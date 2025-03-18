f93605b2df7794fe3f1ff7f9e34bc74c89c7220d
LuisM23 <128239393+LuisMend12@users.noreply.github.com>
2025-03-18 17:01:02 -0400

    n

--- command_line_interface/overloaded_functions.cpp
+++ command_line_interface/overloaded_functions.cpp
@@ -11,10 +11,43 @@ This enables the same function name to perform various tasks based ont he type o
 
 
 
-int addInteger(int a, int b){
-    return a + b;
+// int addInteger(int a, int b){
+//     return a + b;
+// }
+
+// double addDouble(double a, double b) {
+//     return a + b;
+// }
+
+
+/* Overloading by type
+
+
+int add(int a, int b) {
+    return a + b;    
 }
 
-double addDouble(double a, double b) {
+
+double add(double a, double b) {
     return a + b;
+}
+*/
+
+#include <iostream>
+using namespace std;
+const double pi = 3.14159265395;
+
+double calculateArea(double length, double width) {
+    return length * radius;
+}
+
+
+int maini() {
+    double rectangleArea = calculateArea(5.0, 3.0);
+    double circleArea = calculateArea(2.5);
+
+    cout << "Rectangle Area: " << rectangleArea << endl;
+    cout << "Circle Area: " << circleArea << endl;
+    
+    return 0;
 }
